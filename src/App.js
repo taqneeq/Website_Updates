fde0f0eee395cd2264252208adcef24be9867af5
Shayan Zaidi <85441549+Dracula1928@users.noreply.github.com>
2023-01-15 16:51:22 +0530

    Navbar shayan

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <>
+      <div class="menu-wrap">
+        <input type="checkbox" class="toggler"/>
+        <div class="hamburger"><div></div></div>
+        <div class="menu">
+          <div>
+            <div>
+              <ul>
+                <li><a href="#"><b>About</b></a></li>
+                <li><a href="#"><b>Services</b></a></li>
+                <li><a href="#"><b>Project</b></a></li>
+                <li><a href="#"><b>Case Studies</b></a></li>
+                <li><a href="#"><b>Contact</b></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        </div>
+        <header class="showcase">
+    <div class="container showcase-inner">
+      <h1>Taqneeq</h1>
+     
+    </div>
+  </header>
+      </>
+  );
+}
+export default App;
