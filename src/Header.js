42490f293b77f261d2ca7f85361b0a44dc1e2672
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-01-20 01:11:22 +0530

    Main-Event-Page-Code
    Final Code for events page --- Use node version 16 or below --- by using nvm or something else

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Avatar } from "react-lorem-ipsum";
+
+export const Header = () => (
+  <header>
+    <span className="date">Taqneeq Fest</span>
+    <h1>Events</h1>
+   
+  </header>
+);
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const items = [
+  {
+    id: "c",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 80,
+    backgroundColor: "#814A0E"
+  },
+  {
+    id: "f",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 120,
+    backgroundColor: "#959684"
+  },
+  {
+    id: "a",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 260,
+    backgroundColor: "#5DBCD2"
+  },
+  {
+    id: "g",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 200,
+    backgroundColor: "#8F986D"
+  },
+  {
+    id: "d",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 150,
+    backgroundColor: "#FA6779"
+  },
+  {
+    id: "h",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 60,
+    backgroundColor: "#282F49"
+  },
+  {
+    id: "e",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 200,
+    backgroundColor: "#AC7441"
+  },
+  {
+    id: "b",
+    category: "OmegaSUS",
+    title: "Mashru Heada DaddY?",
+    pointOfInterest: 260,
+    backgroundColor: "#CC555B"
+  }
+];
+
+export const openSpring = { type: "spring", stiffness: 200, damping: 30 };
