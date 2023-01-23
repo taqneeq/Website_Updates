9de2b58e9f9af3ba5b2428f066533cc53456472c
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-01-23 22:16:18 +0530

    Final-TQ-Rewind-Timeline
    Final tq rewind cards finished

---
+++ src/App.js
@@ -0,0 +1,197 @@
+import logo from './logo.svg';
+import React,{useEffect} from 'react';
+import './App.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+function App() {
+useEffect(()=>{
+  AOS.init({duration:2000});
+} , []);
+  return (
+    <div className="App">
+      <div class="timeline"> 
+      {/* card 1 */}
+      <div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* card 1 ends */}
+{/* card 2 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 2 ends */}
+{/* card 3 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 3 ends */}
+
+{/* card 4 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 4 ends */}
+{/* card 5 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 5 ends */}
+
+{/* card 6 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 6 ends */}
+
+{/* card 7 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 7 ends */}
+
+{/* card 8 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 8 ends */}
+
+
+{/* card 9 ends */}
+<div  class="timeline__event timeline__event--type1" data-aos="fade-up">
+     <div class="timeline__event__icon ">
+      <i class="lni-cake"></i>
+
+    </div>
+    <div class="timeline__event__date">
+      20-08-2019
+    </div>
+    <div class="timeline__event__content ">
+      <div class="timeline__event__title">
+        LOREM
+      </div>
+      <div class="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+{/* cards 9 ends */}
+</div>
+    </div>
+  );
+}
+
+export default App;
