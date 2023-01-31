998154eb7a976043c6f70a26b479689aa35266c7
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-02-01 02:34:48 +0530

    Final-Events-Page-Changed
    Revamped Events Page
    pending:-
    add content fix alignment

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -7,134 +7,63 @@ export default function Event()
       <>
             <Animate_cursor />
             <div>
-              
-                <div class="bodyevent">
-                    {/* <h1>events</h1> */}
-           <div class="container-event">
-      <img  data-bs-toggle="modal" data-bs-target="#Event1" src="https://images.unsplash.com/photo-1661239916995-7e01eeebabd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1527&q=80" alt=""></img>
-      <img  data-bs-toggle="modal" data-bs-target="#Event2" src="https://images.unsplash.com/photo-1611920504212-25894774afc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80" alt=""></img>
-      <img  data-bs-toggle="modal" data-bs-target="#Event3" src="https://images.unsplash.com/photo-1543351611-58f69d7c1781?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt=""></img>
-      <img  data-bs-toggle="modal" data-bs-target="#Event4" src="https://images.unsplash.com/photo-1609358905581-e5381612486e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1422&q=80" alt=""></img>
-      <img  data-bs-toggle="modal" data-bs-target="#Event5" src="https://images.unsplash.com/photo-1648461513175-7beb9bd09586?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80" alt=""></img>
-      <img  data-bs-toggle="modal" data-bs-target="#Event6" src="https://images.unsplash.com/photo-1617252820692-ff67f50fca2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt=""></img>
+            <div class="hero-section">
+  <div class="card-grid">
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+    <a class="card_event" href="#">
+      <div class="card__background"></div>
+      <div class="card__content">
+        <p class="card__category">Category</p>
+        <h3 class="card__heading">Example Card Heading</h3>
+      </div>
+    </a>
+  
+    
+    
+  </div> 
+  
+        
 
 
-    </div>
-    </div> 
-{/* event 1 modal */}
-    <div class="modal fade" id="Event1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1661239916995-7e01eeebabd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1527&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Pinewood Derby</h1>
-      Participants build and race a soapbox car, applying basic physics and mechanical principals
-      </div>
-      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
+
+
+
+
 </div>
-{/* event 2 modal */}
-<div class="modal fade" id="Event2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1611920504212-25894774afc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Maze Solver</h1>
-      Line tracing cars coded by players have to make their way through a maze. The bot that solves the maze the fastest wins.
-      </div>
-      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-{/* event 3 modal */}
-<div class="modal fade" id="Event3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1543351611-58f69d7c1781?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Smart Football</h1>
-      Participants try to shoot a goal against a robot goalkeeper at the goal post.
-      </div>
-      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-{/* event 4 modal */}
-<div class="modal fade" id="Event4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1609358905581-e5381612486e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1422&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Mission Impossible</h1>
-      Participants dodge the laser beams and cross the three levels, each harder than the previous one      </div>
-      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-{/* events 5 modal */}
-<div class="modal fade" id="Event5" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1648461513175-7beb9bd09586?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Piano tiles</h1>
-      Players walk on tiles creating the notes of piano as they walk
-      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
-</div> 
-</div>
-{/* event 6 modal */}
-<div class="modal fade" id="Event6" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      <img class="card-img-top" src="https://images.unsplash.com/photo-1617252820692-ff67f50fca2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="Card image cap"></img>
-      </div>
-      <div class="modal-body">
-      <h1>Earthquake proof</h1>
-      Participants have to build an earthquake-proof building using sticks, straws and other materials given which can last the longest in the earthquake simulator.      <div class="modal-footer">
-        <a type="button" class="btn btn-primary" href="#">Register</a>
-        <button type="button" class="btn btn-secondary" data-dismiss="Event1">Close</button>
-      </div>
-    </div>
-  </div>
-</div> 
-</div>
-
-
-
-
-
-
-
-
     </div>
     </>
     
